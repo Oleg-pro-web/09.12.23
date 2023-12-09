4c881a9a48aec4c35e1420b07abc9b82b771ac51
Oleg <andrishkooleg@gmail.com>
2023-12-09 10:28:48 +0100

    modified:   src/App.js 	renamed:    src/components/recipe/RecipeInfo.jsx -> src/components/RecipeInfo/RecipeInfo.jsx 	modified:   src/components/recipe/Recipe.jsx 	deleted:    src/logo.svg

--- src/components/recipe/Recipe.jsx
+++ src/components/recipe/Recipe.jsx
@@ -1,6 +1,18 @@
+import { RecipeInfo } from "../RecipeInfo/RecipeInfo"
 export function Recipe({ recipe }) {
     return (
         < ul >
+            {recipe.map(({ name, time, servings, calories, difficulty, img }) => {
+                return <RecipeInfo
+                    key={name}
+                    name={name}
+                    time={time}
+                    servings={servings}
+                    calories={calories}
+                    difficulty={difficulty}
+                    img={img}
+                />
+            })}
         </ul >
     )
 }
